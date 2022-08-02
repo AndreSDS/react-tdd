7ace3f9d5f0b0510c148912c231e80e31242c50c
andresds <andre_smiths@outlook.com>
2022-08-02 15:10:52 -0300

    creating first tests

--- __tests__/SignUpPage.test.tsx
+++ __tests__/SignUpPage.test.tsx
@@ -1,15 +1,79 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { SignUp } from "../src/Pages/SignUp";
 
-describe("SignUpPage", () => {
+describe("SignUp Page", () => {
+  let wrapperContainer: HTMLElement;
   beforeEach(() => {
-    render(<SignUp />);
+    const { container } = render(<SignUp />);
+
+    wrapperContainer = container;
   });
 
-  it("should has header", () => {
-    const header = screen.queryByRole("heading", { name: "Sign Up" });
-    expect(header).toBeInTheDocument();
+  describe("Layout", () => {
+    it("should have header", () => {
+      const header = screen.queryByRole("heading", { name: "Sign Up" });
+      expect(header).toBeInTheDocument();
+    });
+
+    it("should have username input", () => {
+      const inputUserName = screen.getByLabelText("Username");
+      expect(inputUserName).toBeInTheDocument();
+    });
+
+    it("should have email input", () => {
+      const inputEmail = screen.getByLabelText("E-mail");
+      expect(inputEmail).toBeInTheDocument();
+    });
+
+    it("should have password input", () => {
+      const inputPassword = screen.getByLabelText("Password");
+      expect(inputPassword).toBeInTheDocument();
+    });
+
+    it("should have password type for password input", () => {
+      const inputPassword = screen.getByLabelText("Password");
+      expect(inputPassword).toHaveAttribute("type", "password");
+    });
+
+    it("should have password repeat input", () => {
+      const inputPasswordRepeat = screen.getByLabelText("Password Repeat");
+      expect(inputPasswordRepeat).toBeInTheDocument();
+    });
+
+    it("should have password repeat type for password repeat input", () => {
+      const inputPasswordRepeat = screen.getByLabelText("Password Repeat");
+      expect(inputPasswordRepeat).toHaveAttribute("type", "password");
+    });
+
+    it("should have submit button", () => {
+      const submitButton = screen.getByRole("button", { name: "Sign Up" });
+      expect(submitButton).toBeInTheDocument();
+    });
+
+    it("should have submit button type submit", () => {
+      const submitButton = screen.getByRole("button", { name: "Sign Up" });
+      expect(submitButton).toHaveAttribute("type", "submit");
+    });
+
+    it("should have disabled the button initially", () => {
+      const submitButton = screen.getByRole("button", { name: "Sign Up" });
+      expect(submitButton).toBeDisabled();
+    });
+  });
+
+  describe("Behavior", () => {
+    it("should have enabled the button when inputs password and password-repeat have same value", () => {
+      const inputPassword = screen.getByLabelText("Password");
+      const inputPasswordRepeat = screen.getByLabelText("Password Repeat");
+      const submitButton = screen.getByRole("button", { name: "Sign Up" });
+
+      userEvent.type(inputPassword, "123456");
+      userEvent.type(inputPasswordRepeat, "123456");
+
+      expect(submitButton).toBeEnabled();
+    });
   });
 });
--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -1,9 +1,86 @@
-import React from "react";
+import React, {
+  ChangeEvent,
+  ChangeEventHandler,
+  useEffect,
+  useState,
+} from "react";
 
 export const SignUp = () => {
+  const [disabled, setDisabled] = useState(true);
+  const [formValues, setFormValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    password_repeat: "",
+  });
+
+  const onChangeInputValue = (e: ChangeEvent<HTMLInputElement> | undefined) => {
+    if (e) {
+      const { value, name } = e.currentTarget;
+      setFormValues({ ...formValues, [name]: value });
+    }
+  };
+
+  const disableButton =
+    formValues.password !== "" &&
+    formValues.password === formValues.password_repeat
+      ? false
+      : true;
+
+  const disabledButton = () => {
+    if (
+      formValues.password !== "" &&
+      formValues.password === formValues.password_repeat
+    ) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
+  useEffect(() => {
+    disabledButton();
+  }, [formValues.password, formValues.password_repeat]);
+
   return (
     <div>
       <h1>Sign Up</h1>
+
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        id="username"
+        name="usename"
+        onChange={onChangeInputValue}
+      />
+
+      <label htmlFor="email">E-mail</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        onChange={onChangeInputValue}
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        onChange={onChangeInputValue}
+      />
+
+      <label htmlFor="password_repeat">Password Repeat</label>
+      <input
+        type="password"
+        id="password_repeat"
+        name="password_repeat"
+        onChange={onChangeInputValue}
+      />
+
+      <button disabled={disabled} type="submit">
+        Sign Up
+      </button>
     </div>
   );
 };
