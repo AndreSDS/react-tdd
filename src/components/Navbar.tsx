58a433f155bad386fe68ce174f1b883187a6d557
andresds <andre_smiths@outlook.com>
2022-09-07 13:41:00 -0300

    removing function onClick from Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,20 +9,13 @@ interface NavBarProps {
 export const Navbar = ({ handlePath }: NavBarProps) => {
   const { t } = useTranslation();
 
-  const onClick = (event: any) => {
-    event.preventDefault();
-    const path = event.currentTarget.getAttribute("href");
-    window.history.pushState({}, "", path);
-    handlePath(path);
-  };
-
   return (
     <nav className="bg-white mb-3 px-2 sm:px-4 py-2.5 dark:bg-gray-400 w-full">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link
           to="/"
           title={t("home")}
-          onClick={onClick}
+          // onClick={onClick}
           className="flex flex-1 items-center"
         >
           <img
@@ -38,7 +31,6 @@ export const Navbar = ({ handlePath }: NavBarProps) => {
             className="flex items-center ml-3"
             to="/signup"
             title={t("signUp")}
-            onClick={onClick}
           >
             {t("signUp")}
           </Link>
@@ -46,7 +38,6 @@ export const Navbar = ({ handlePath }: NavBarProps) => {
             className="flex items-center ml-3"
             to="/login"
             title={t("login")}
-            onClick={onClick}
           >
             {t("login")}
           </Link>
